16cadf11f8f653205be34df1165201923cec4ae6
Víctor Sánchez Lara <vslgasol@gmail.com>
2024-11-05 12:47:27 +0100

    Update compound_by_chemical_formula.py

--- compound_by_chemical_formula.py
+++ compound_by_chemical_formula.py
@@ -5,42 +5,44 @@ import csv
 
 
 def generate_refrative_index_compound(compound, density, name = None):
-    Na = 6.02214129*10**(23)
-    re = 2.8179403*10**(-15) * 100 # cm
+    Na = 6.02214129*10**(23) # Avogadro number
+    re = 2.8179403*10**(-15) * 100 # Classical electron radius (cm)
     current_path = os.path.dirname(__file__)
-    path_index = os.path.join(current_path,'form_factors')
-    final_energies = np.linspace(1, 150, 500)
-    elements_count = parse_compound_formula(compound)
+    path_index = os.path.join(current_path,'form_factors') # Path to form_factors folder
+    final_energies = np.linspace(1, 150, 500) # Array with the energies for which we want to obtain the refractive index
+    elements_count = parse_compound_formula(compound) # Read the chemical formula of the compound
+    
+    # Initialization
     f1 = 0
     f2 = 0
     mu = 0
     Ma = 0
     
     for key, value in elements_count.items():
-        
+        # For each element in the compound, the form factor file is read
         element = np.loadtxt(os.path.join(path_index,key+'.txt'))
         
-        f1_element =  element[:,1]
-        f2_element =  element[:,2]
-        mu_element = element[:,5]
+        f1_element =  element[:,1] # atomic form factor f1
+        f2_element =  element[:,2] # atomic form factor f2
+        mu_element = element[:,5] # mass attenuation coefficient
         
-        original_energies = element[:,0]
+        original_energies = element[:,0] # energies of the text file
 
-        f1_elements_interpolated = interpolate_f(original_energies, f1_element, final_energies)
-        f2_elements_interpolated = interpolate_f(original_energies, f2_element, final_energies)
-        mu_element_interpolated = interpolate_f(original_energies, mu_element, final_energies)
+        f1_elements_interpolated = interpolate_f(original_energies, f1_element, final_energies) # f1 for final_energies
+        f2_elements_interpolated = interpolate_f(original_energies, f2_element, final_energies) # f2 for final_energies
+        mu_element_interpolated = interpolate_f(original_energies, mu_element, final_energies) # mass attenuation coeff. for final_energies
 
         with open(os.path.join(current_path,'Elements.csv')) as element_csv:
             csvFile = csv.reader(element_csv)
             for row in csvFile:
                 try:
-                    if row[2] == key:
-                        density_element = float(row[4])
-                        Ma_element = float(row[3])                    
+                    if row[2] == key: # If element name matches with the name of Elements.csv retrieves the density and molar mass
+                        density_element = float(row[4]) # density (g/cm3)
+                        Ma_element = float(row[3]) # molar mass                   
                 except:
                     continue
         
-        f1 += value * f1_elements_interpolated
+        f1 += value * f1_elements_interpolated 
         f2 += value * f2_elements_interpolated
         mu += value * Ma_element* mu_element_interpolated 
         Ma += value * Ma_element
@@ -138,7 +140,7 @@ def linear_interpolation(x,x0,x1,y0,y1):
     return y
 
 if __name__ == '__main__':
-    compound = 'H2O'
-    density = 1. # hay que meterlo como input
+    compound = 'H2O' # Chemical Formula
+    density = 1. # density in g/cm3
     file, filename = generate_refrative_index_compound(compound, density)
     save_file(file, filename)
